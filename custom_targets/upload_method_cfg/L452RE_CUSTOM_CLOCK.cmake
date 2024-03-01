eea7fc85efecedca0fb5881a93727395dc7902f0
JohnK1987 <Odiin@seznam.cz>
2024-03-01 14:04:22 +0100

    Update L452RE_CUSTOM_CLOCK.cmake

--- custom_targets/upload_method_cfg/L452RE_CUSTOM_CLOCK.cmake
+++ custom_targets/upload_method_cfg/L452RE_CUSTOM_CLOCK.cmake
@@ -7,6 +7,8 @@ set(UPLOAD_METHOD_DEFAULT OPENOCD)
 
 # Config options for OPENOCD
 # -------------------------------------------------------------
+# We set TRUE to enable OpenOCD
 set(OPENOCD_UPLOAD_ENABLED TRUE)
+# Here set a path to requested OpenOCD config. In this case we use already implemented one.
 set(OPENOCD_CHIP_CONFIG_COMMANDS
-    -f ${CMAKE_CURRENT_LIST_DIR}/openocd_cfgs/stm32l452re.cfg)
+    -f ${CMAKE_SOURCE_DIR}/mbed-os/targets/upload_method_cfg/openocd_cfgs/s/stm32l452re.cfg)
